d47c61f06a59f7d40638299afe952b92395512da
Nick Jalbert <nick@jalbert.io>
2021-01-22 17:02:50 -0800

    Separate format and lint github actions

--- scripts/format_code.py
+++ scripts/format_code.py
@@ -3,23 +3,36 @@ Run black code formatter on AgentOS Python files
 
 To use::
 
-  $ python documentation/format_code.py
+  # Format all code
+  $ python scripts/format_code.py
+
+  # Print files that will be formatted, but don't actually format
+  $ python scripts/format_code.py --check
 """
 
 import os
+import sys
 from subprocess import run
 from subprocess import PIPE
+from subprocess import STDOUT
 
 from shared import root_dir
 from shared import traverse_tracked_files
 
+returncode = 0
+
 
 def format_file(path):
+    global returncode
     extension = os.path.splitext(path)[1]
     if extension != ".py":
         return
     cmd = ["black", "--line-length=79", path]
-    out = run(cmd, stdout=PIPE).stdout.decode("utf-8")
+    if len(sys.argv) > 1 and sys.argv[1] == "--check":
+        cmd.append("--check")
+    result = run(cmd, stdout=PIPE, stderr=STDOUT)
+    returncode = returncode | result.returncode
+    out = result.stdout.decode("utf-8")
     if out:
         print(path)
         print(out)
@@ -27,3 +40,4 @@ def format_file(path):
 
 
 traverse_tracked_files(root_dir, format_file)
+sys.exit(returncode)
