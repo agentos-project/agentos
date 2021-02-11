fb7f66a033ba389cb518ccd3fe17ba324ad48252
Nick Jalbert <nick@jalbert.io>
2021-02-11 15:11:33 -0800

    Initial port of CLI to new abstractions
    - agentos {init, train, test}
    - agent.ini file
    - functional basic agent

--- agentos/__init__.py
+++ agentos/__init__.py
@@ -1,6 +1,22 @@
 """The ``agentos`` module provides an API for building learning agents."""
 
 from agentos.version import VERSION as __version__  # noqa: F401
-from agentos.core import Agent, Policy, run_agent, rollout, rollouts
+from agentos.core import (
+    Agent,
+    Policy,
+    Environment,
+    Trainer,
+    run_agent,
+    rollout,
+    rollouts,
+)
 
-__all__ = ["Agent", "Policy", "run_agent", "rollout", "rollouts"]
+__all__ = [
+    "Agent",
+    "Policy",
+    "Trainer",
+    "Environment",
+    "run_agent",
+    "rollout",
+    "rollouts",
+]
