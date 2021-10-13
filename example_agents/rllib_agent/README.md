d5c8a44db5d62e72298054c20de87cea123655f4
Andy Konwinski <andykonwinski@gmail.com>
2021-10-13 14:01:31 -0700

    RLlib agent working using torch (used to be TF)

--- example_agents/rllib_agent/README.md
+++ example_agents/rllib_agent/README.md
@@ -2,15 +2,8 @@
 
 This directory is a functional AgentOS agent.
 
-It is also an MLflow Project.
-
-You can run the project directly (see `python main.py --help`)
-or via MLlfow (`pip install mlflow; mlflow run .`).
-
-If you are going to do any developing on this Agent, I recommend you create a conda
-env to use for dev -- and update it whenever necessary -- per the following instructions:
-* Start by creating a conda env using the conda_env.yaml file via something like:
-  `conda env create --file conda_env.yaml`
-* Then, whenever you need to add or change dependencies, don't use pip or
-  other tools directly inside your env. Instead, update conda_env.yaml, and
-  then run: `conda env update --file conda_env.yaml --prune`
+Run the project from the CLI from within the current directory:
+```
+pip install -r requirements.txt
+agentos run agent
+```
--- example_agents/rllib_agent/main.py
+++ example_agents/rllib_agent/main.py
@@ -2,42 +2,48 @@ import ray
 import ray.rllib.agents.registry as rllib_registry
 from ray.tune.registry import register_env as rllib_reg_env
 from ray.rllib.agents.trainer import COMMON_CONFIG
+from ray.rllib.utils import merge_dicts
 
 
 class RLlibAgent:
-    def __init__(self, algo_name="PPO", trainer_config=COMMON_CONFIG):
+    def __init__(self, algo_name="PPO", trainer_config=None):
         """Init a Ray agent with the given env_class and an algorithm name.
 
         :param algo_name: A string. For valid values of algo_name, see
         https://github.com/ray-project/ray/blob/master/rllib/agents/registry.py
         """
-        env_class = self.env.unwrapped.__class__
+        if not trainer_config:
+            trainer_config = merge_dicts(
+                COMMON_CONFIG,
+                {
+                    "framework": "torch"
+                })
         if not ray.is_initialized():
             ray.init()
+        env_class = self.env.unwrapped.__class__
         trainer_class = rllib_registry.get_trainer_class(algo_name)
-        print()
-        print(type(trainer_class))
 
         def wrapper(conf):
             return env_class()  # Ray requires env.__init__() take config.
-
         rllib_reg_env(env_class.__name__, wrapper)
         self.ray_trainer = trainer_class(
-            config=self.trainer_config, env=env_class.__name__
+            config=trainer_config, env=env_class.__name__
         )
-        self._last_obs = self.init_obs
-        self.done = False
 
     def evaluate(self):
-        if self.done:
-            return True
-        action = self.ray_trainer.compute_action(self._last_obs)
-        self._last_obs, _, self.done, _ = self.env.step(action)
-        return self.done
+        eval_env = self.env.__class__()
+        last_obs = eval_env.reset()
+        done = False
+        while not done:
+            action = self.ray_trainer.compute_action(last_obs)
+            print(f"decided on action {action}")
+            last_obs, _, done, _ = eval_env.step(action)
+            print(f"took step, got observation {last_obs}, done = {done}")
 
-    def train(self, num_iterations):
+    def learn(self, num_iterations: int):
         """Causes Ray to learn"""
-        self.ray_agent.train(num_iterations)
+        for _ in range(int(num_iterations)):
+            self.ray_trainer.train()
 
 
 def test_rllib_agent():
