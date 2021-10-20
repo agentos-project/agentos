a443b3e8eb9a5e205c5cce0e4fae898896a374ea
Andy Konwinski <andykonwinski@gmail.com>
2021-10-19 19:57:31 -0700

    tweak tests

---
+++ tests/example_agents/test_acme_r2d2.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from tests.utils import run_component_in_dir
+
+
+def test_acme_r2d2_agent(venv):
+    agent_dir = (
+        Path(__file__).parent.parent.parent / "example_agents" / "acme_r2d2"
+    )
+    run_component_in_dir(
+        agent_dir, venv, "agent", entry_points=["evaluate", "learn"],
+        entry_point_params=[
+            "--param-file parameters.yaml",
+            "--param-file parameters.yaml"
+        ]
+    )
