f62dd098cff891751b33a5e3f79c065f6c964141
Nick Jalbert <nick@jalbert.io>
2021-12-21 17:08:35 -0500

    Lint and format

--- agentos/templates/agent.py
+++ agentos/templates/agent.py
@@ -5,11 +5,7 @@ import agentos
 # A basic agent.
 class {agent_name}(agentos.Agent):
     DEFAULT_ENTRY_POINT = "evaluate"
-    def learn(self,
-              num_episodes=10,
-              test_every=True,
-              test_num_episodes=5):
+
+    def learn(self, num_episodes=10, test_every=True, test_num_episodes=5):
         """Set up some defaults params for this entry point."""
-        super().learn(num_episodes,
-                      test_every,
-                      test_num_episodes)
+        super().learn(num_episodes, test_every, test_num_episodes)
