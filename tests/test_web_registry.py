6b4e61ae5f6d6517ecaa87b24e18b0640938efec
Andy Konwinski <andykonwinski@gmail.com>
2022-02-03 13:40:37 -0800

    allow easy swapping of target branch for tests that use code from github

--- tests/test_web_registry.py
+++ tests/test_web_registry.py
@@ -2,6 +2,7 @@ import pytest
 from agentos.registry import WebRegistry
 from agentos.component import Component
 from agentos.repo import Repo
+from tests.utils import TESTING_BRANCH_NAME
 
 agentos_repo_spec = {
     "AgentOSRepo": {
@@ -60,7 +61,7 @@ def test_web_registry():
     # Test adding a component that we generate from the repo.
     simple_component = Component.from_repo(
         repo,
-        identifier="SimpleComponent==test_staging",
+        identifier=f"SimpleComponent=={TESTING_BRANCH_NAME}",
         class_name="SimpleComponent",
         file_path="tests/test_web_registry.py",
     )
@@ -69,26 +70,27 @@ def test_web_registry():
     web_registry.add_component_spec(simple_component.to_spec())
 
     # Test getting a flattened component (i.e., the one we just added)
-    comp = web_registry.get_component_spec(
-        name="SimpleComponent", version="test_staging", flatten=True
+    flat_comp_spec = web_registry.get_component_spec(
+        name="SimpleComponent", version=TESTING_BRANCH_NAME, flatten=True
     )
-    assert comp["name"] == "SimpleComponent"
-    assert comp["repo"] == "AgentOSRepo"
+    assert flat_comp_spec["name"] == "SimpleComponent"
+    assert flat_comp_spec["repo"] == "AgentOSRepo"
 
     # Test getting an unflattened component (i.e., the one we just added)
-    comp = web_registry.get_component_spec(
-        name="SimpleComponent", version="test_staging", flatten=False
+    nested_comp_spec = web_registry.get_component_spec(
+        name="SimpleComponent", version=TESTING_BRANCH_NAME, flatten=False
     )
-    assert comp["SimpleComponent"]["class_name"] == "SimpleComponent"
-    assert comp["SimpleComponent"]["repo"] == "AgentOSRepo"
+    full_id = f"SimpleComponent=={TESTING_BRANCH_NAME}"
+    assert nested_comp_spec[full_id]["class_name"] == "SimpleComponent"
+    assert nested_comp_spec[full_id]["repo"] == "AgentOSRepo"
 
     # Test adding a ComponentRun
     param_set = {"SimpleComponent": {"add_to_init_member": {"i": 10}}}
-    comp_run = simple_component.run("SimpleComponent", param_set)
+    comp_run = simple_component.run("add_to_init_member", param_set)
     web_registry.add_run_command_spec(comp_run.run_command)
 
 
-# TODO: add a test that publishes a ComponentRun or Compoonent from the CLI.
+# TODO: add a test that publishes a ComponentRun or Component from the CLI.
 # def test_web_registry_from_cli():
 #     from tests.utils import run_test_command
 #     run_test_command()
