2c047a29a9514d15dc5892fde67bdc4561fd4cd6
Andy Konwinski <andykonwinski@gmail.com>
2022-02-05 15:07:47 -0800

    more updates to acme model save/load and update r2d2 to use it

--- example_agents/acme_dqn/network.py
+++ example_agents/acme_dqn/network.py
@@ -6,6 +6,44 @@ from pathlib import Path
 from agentos.run import Run
 
 
+class TFModelSaver:
+    """
+    Handles saving and restoring TF models to/from Runs.
+    Can be used by different network components.
+    """
+    @staticmethod
+    def save(save_as_name: str, network: tf.Module, run=None):
+        dir_path = Path(tempfile.mkdtemp())
+        checkpoint = tf.train.Checkpoint(module=network)
+        checkpoint.save(dir_path / save_as_name / save_as_name)
+        if run:
+            run.log_artifact(dir_path / save_as_name)
+        shutil.rmtree(dir_path)
+
+    @staticmethod
+    def restore(save_as_name: str, network: tf.Module):
+        runs = Run.get_all_runs()
+        for run in runs:
+            try:
+                save_path = Path(run.download_artifacts(save_as_name))
+                if save_path.is_dir():
+                    checkpoint = tf.train.Checkpoint(module=network)
+                    latest = tf.train.latest_checkpoint(save_path)
+                    if latest is not None:
+                        checkpoint.restore(latest)
+                        print(
+                            f"AcmeRunManager: Restored Tensorflow model "
+                            f"{save_as_name}."
+                        )
+                        return
+            except IOError as e:
+                print(f"failed to download artifacts: {e}")
+        print(
+            f"AcmeRunManager: No saved Tensorflow model "
+            f"{save_as_name} found."
+        )
+
+
 class AcmeDQNNetwork:
     def __init__(self):
         self.net = snt.Sequential(
@@ -20,31 +58,7 @@ class AcmeDQNNetwork:
         self.restore()
 
     def save(self, run=None):
-        dir_path = Path(tempfile.mkdtemp())
-        checkpoint = tf.train.Checkpoint(module=self.net)
-        checkpoint.save(dir_path / self.save_as_name / self.save_as_name)
-        if run:
-            run.log_artifact(dir_path / self.save_as_name)
-        shutil.rmtree(dir_path)
+        TFModelSaver.save(self.save_as_name, self.net, run=run)
 
     def restore(self):
-        runs = Run.get_all_runs()
-        for run in runs:
-            try:
-                save_path = Path(run.download_artifacts(self.save_as_name))
-                if save_path.is_dir():
-                    checkpoint = tf.train.Checkpoint(module=self.net)
-                    latest = tf.train.latest_checkpoint(save_path)
-                    if latest is not None:
-                        checkpoint.restore(latest)
-                        print(
-                            f"AcmeRunManager: Restored Tensorflow model "
-                            f"{self.save_as_name}."
-                        )
-                        return
-            except IOError as e:
-                print(f"failed to download artifacts: {e}")
-        print(
-            f"AcmeRunManager: No saved Tensorflow model "
-            f"{self.save_as_name} found."
-        )
+        TFModelSaver.restore(self.save_as_name, self.net)
