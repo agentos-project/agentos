1f2b8dca8d133c5eaf599b753ae73970619031de
Andy Konwinski <andykonwinski@gmail.com>
2022-02-14 11:27:31 -0800

    run black

--- web/registry/migrations/0001_initial.py
+++ web/registry/migrations/0001_initial.py
@@ -8,93 +8,176 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Component',
+            name="Component",
             fields=[
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('identifier', models.CharField(max_length=200, primary_key=True, serialize=False)),
-                ('name', models.CharField(max_length=200)),
-                ('version', models.CharField(max_length=200)),
-                ('file_path', models.TextField()),
-                ('class_name', models.CharField(max_length=200)),
-                ('instantiate', models.BooleanField()),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                (
+                    "identifier",
+                    models.CharField(
+                        max_length=200, primary_key=True, serialize=False
+                    ),
+                ),
+                ("name", models.CharField(max_length=200)),
+                ("version", models.CharField(max_length=200)),
+                ("file_path", models.TextField()),
+                ("class_name", models.CharField(max_length=200)),
+                ("instantiate", models.BooleanField()),
             ],
         ),
         migrations.CreateModel(
-            name='Repo',
+            name="Repo",
             fields=[
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('identifier', models.CharField(max_length=200, primary_key=True, serialize=False)),
-                ('type', models.CharField(max_length=200)),
-                ('url', models.CharField(max_length=200)),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                (
+                    "identifier",
+                    models.CharField(
+                        max_length=200, primary_key=True, serialize=False
+                    ),
+                ),
+                ("type", models.CharField(max_length=200)),
+                ("url", models.CharField(max_length=200)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='RunCommand',
+            name="RunCommand",
             fields=[
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('identifier', models.CharField(max_length=200, primary_key=True, serialize=False)),
-                ('entry_point', models.CharField(max_length=200)),
-                ('parameter_set', models.JSONField(default=dict)),
-                ('component', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='registry.component')),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                (
+                    "identifier",
+                    models.CharField(
+                        max_length=200, primary_key=True, serialize=False
+                    ),
+                ),
+                ("entry_point", models.CharField(max_length=200)),
+                ("parameter_set", models.JSONField(default=dict)),
+                (
+                    "component",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="registry.component",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='Run',
+            name="Run",
             fields=[
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('identifier', models.CharField(max_length=200, primary_key=True, serialize=False)),
-                ('info', models.JSONField(default=dict)),
-                ('data', models.JSONField(default=dict)),
-                ('artifact_tarball', models.FileField(null=True, upload_to='artifact_tarballs/')),
-                ('agent', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='runs_as_agent', to='registry.component')),
-                ('environment', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='runs_as_environment', to='registry.component')),
-                ('run_command', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='registry.runcommand')),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                (
+                    "identifier",
+                    models.CharField(
+                        max_length=200, primary_key=True, serialize=False
+                    ),
+                ),
+                ("info", models.JSONField(default=dict)),
+                ("data", models.JSONField(default=dict)),
+                (
+                    "artifact_tarball",
+                    models.FileField(
+                        null=True, upload_to="artifact_tarballs/"
+                    ),
+                ),
+                (
+                    "agent",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="runs_as_agent",
+                        to="registry.component",
+                    ),
+                ),
+                (
+                    "environment",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="runs_as_environment",
+                        to="registry.component",
+                    ),
+                ),
+                (
+                    "run_command",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="registry.runcommand",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='ComponentDependency',
+            name="ComponentDependency",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('attribute_name', models.TextField()),
-                ('dependee', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='dependee_set', to='registry.component')),
-                ('depender', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='depender_set', to='registry.component')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                ("attribute_name", models.TextField()),
+                (
+                    "dependee",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="dependee_set",
+                        to="registry.component",
+                    ),
+                ),
+                (
+                    "depender",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="depender_set",
+                        to="registry.component",
+                    ),
+                ),
             ],
             options={
-                'unique_together': {('depender', 'dependee', 'attribute_name')},
+                "unique_together": {
+                    ("depender", "dependee", "attribute_name")
+                },
             },
         ),
         migrations.AddField(
-            model_name='component',
-            name='dependencies',
-            field=models.ManyToManyField(through='registry.ComponentDependency', to='registry.Component'),
+            model_name="component",
+            name="dependencies",
+            field=models.ManyToManyField(
+                through="registry.ComponentDependency", to="registry.Component"
+            ),
         ),
         migrations.AddField(
-            model_name='component',
-            name='repo',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='repos', to='registry.repo'),
+            model_name="component",
+            name="repo",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="repos",
+                to="registry.repo",
+            ),
         ),
         migrations.AlterUniqueTogether(
-            name='component',
-            unique_together={('name', 'version')},
+            name="component",
+            unique_together={("name", "version")},
         ),
     ]
