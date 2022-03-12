feb593927b666bbae31d835836fff907b273242d
Andy Konwinski <andykonwinski@gmail.com>
2022-03-12 12:42:36 -0800

    run format, tweak ilya demo

--- demo_script_sb3.py
+++ demo_script_sb3.py
@@ -1,28 +1,26 @@
-from agentos.repo import Repo
-from agentos.registry import Registry
-from agentos.component import Component
 from agentos.argument_set import ArgumentSet
-from agentos.registry import WebRegistry
+from agentos.component import Component
 from agentos.component_run import ComponentRun
+from agentos.registry import Registry, WebRegistry
+from agentos.repo import Repo
 
-
-#sb3_repo = Repo.from_github("DLR-RM", "stable-baselines3")
-#sb3_reg = Registry.from_repo(sb3_repo)
+# sb3_repo = Repo.from_github("DLR-RM", "stable-baselines3")
+# sb3_reg = Registry.from_repo(sb3_repo)
 sb3_reg = Registry.from_yaml("/tmp/sb3_registry_inferred.yaml")
 
-sb3_comp = Component.from_registry(sb3_reg, "module:stable_baselines3____init__.py")
+sb3_comp = Component.from_registry(
+    sb3_reg, "module:stable_baselines3____init__.py"
+)
 sb3_comp.class_name = "PPO"
 sb3_comp.instantiate = True
-arg_set = ArgumentSet({
+arg_set = ArgumentSet(
+    {
         "module:stable_baselines3____init__.py": {
-            "__init__": {
-                "policy": 'MlpPolicy', "env": "CartPole-v1"
-            },
-            "learn": {
-                "total_timesteps": 10000
-            }
+            "__init__": {"policy": "MlpPolicy", "env": "CartPole-v1"},
+            "learn": {"total_timesteps": 10000},
         }
-    })
+    }
+)
 run = sb3_comp.run_with_arg_set("learn", arg_set, log_return_value=False)
 
 # Write a run to your local WebRegistry, then read it back in.
@@ -41,5 +39,5 @@ loaded_run.run_command.run()
 
 # Make sure we can write a run that was read in from a file registry
 # out to a WebRegistry.
-#loaded_run.to_registry(wr)
-#ComponentRun.from_registry(wr, loaded_run.identifier)
+# loaded_run.to_registry(wr)
+# ComponentRun.from_registry(wr, loaded_run.identifier)
