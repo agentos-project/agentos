8c2bde93d7a33fa7ec35c44dde329be5dda965a5
Andy Konwinski <andykonwinski@gmail.com>
2022-03-24 19:20:44 -0700

    update TESTING_BRANCH_NAME to test_prod

--- tests/utils.py
+++ tests/utils.py
@@ -21,7 +21,7 @@ TESTING_GITHUB_REPO = "agentos"
 TESTING_GITHUB_REPO_URL = (
     f"https://github.com/{TESTING_GITHUB_ACCOUNT}/{TESTING_GITHUB_REPO}"
 )
-TESTING_BRANCH_NAME = "test_staging"
+TESTING_BRANCH_NAME = "test_prod"
 
 
 def run_test_command(cmd, cli_args=None, cli_kwargs=None):
