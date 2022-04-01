8013ae73554251252914def3e3cb08e24a825016
Andy Konwinski <andykonwinski@gmail.com>
2022-03-31 18:57:30 -0700

    add demo script for running random agent from agentos github repos example_agents

---
+++ documentation/demos/agentos_github_random_agent.py
@@ -0,0 +1,42 @@
+from pcs.component import Component
+from pcs.component_run import ComponentRun
+from pcs.registry import WebRegistry
+from pcs.repo import Repo
+
+
+# download agent Component, its dependency components, and wire them up.
+repo = Repo.from_github("agentos-project", "agentos")
+c_suff = "==master"
+f_pref = "example_agents/random/"
+ag_c = Component.from_repo(repo, f"a{c_suff}", f"{f_pref}agent.py")
+env_c = Component.from_repo(repo, f"e{c_suff}", f"{f_pref}environment.py")
+pol_c = Component.from_repo(repo, f"p{c_suff}", f"{f_pref}policy.py")
+ds_c = Component.from_repo(repo, f"d{c_suff}", f"{f_pref}dataset.py")
+
+env_c.instantiate = True
+env_c.class_name = "Corridor"
+
+ag_c.instantiate = True
+ag_c.class_name = "BasicAgent"
+ag_c.add_dependency(env_c, "environment")
+
+pol_c.instantiate = True
+pol_c.class_name = "RandomPolicy"
+ag_c.add_dependency(pol_c, "policy")
+pol_c.add_dependency(env_c, "environment")
+
+ds_c.instantiate = True
+ds_c.class_name = "BasicDataset"
+ag_c.add_dependency(ds_c, "dataset")
+
+
+# Run the agent component
+r = ag_c.run_with_arg_set("run_episode", {})
+
+# Publish the agent run to the local WebRegistry
+wr = WebRegistry("http://localhost:8000/api/v1")
+r.to_registry(wr)
+
+# Now load it back in from the WebRegistry and run it.
+loaded_run = ComponentRun.from_registry(wr, r.identifier)
+loaded_run.run_command.run()
