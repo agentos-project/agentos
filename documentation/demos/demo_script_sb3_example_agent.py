86dbe6922b3c670548bb06352be1528688ac54cf
Andy Konwinski <andykonwinski@gmail.com>
2022-04-11 13:44:50 -0700

    add more instructions for publshing AgentRun to demo

--- documentation/demos/demo_script_sb3_example_agent.py
+++ documentation/demos/demo_script_sb3_example_agent.py
@@ -1,3 +1,4 @@
+from agentos import AgentRun
 from pcs import Component
 from pcs.registry import WebRegistry
 
@@ -6,4 +7,17 @@ random_agent = Component.from_registry_file(
 ).to_versioned_component()
 run = random_agent.run_with_arg_set("evaluate", {})
 wr = WebRegistry("http://localhost:8000/api/v1")
-run.to_registry(wr)
+
+# You can publish the ComponentRun to locally running WebRegistry server
+# via the following command:
+# run.to_registry(wr)
+
+# OR, update and uncomment the code below to publish the AgentRun itself.
+# Look at the output printed by the call to `run_with_arg_set()` above,
+# and copy the ID of the AgentRun from the line that looks similar to this:
+# "Results for AgentRun 85ad300d7d86461c938444063de68343"
+#agent_run_id = XXXXXXXXXXXXXXXXXXXXXXX
+#from pcs.registry import WebRegistry
+#sb3_agent_run = AgentRun.from_existing_run_id(agent_run_id)
+#wr = WebRegistry("http://localhost:8000/api/v1")
+#sb3_agent_run.to_registry(wr)
