baae07da1b5cd2da6518d394313670f146b448dc
Nick Jalbert <nick@jalbert.io>
2022-04-13 22:41:53 +0600

    Regression test for broken dulwich checkout
    Issue #348

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pcs.component import Component
 from pcs.repo import LocalRepo, Repo
 from tests.utils import (
@@ -43,3 +45,20 @@ def test_local_to_from_registry():
     repo_from_reg = Repo.from_registry(reg, "test_id")
     assert repo.identifier == repo_from_reg.identifier
     assert repo.local_dir == repo_from_reg.local_dir
+
+
+def test_repo_checkout_bug():
+    aos_repo = Repo.from_github("agentos-project", "agentos")
+    pcs_component_path = Path("pcs") / Path("component.py")
+    # pcs/component.py exists in 4e12203
+    exists_version = "4e12203faaf84361af9432271e013ddfb927f75d"
+    exists_path = aos_repo.get_local_file_path(
+        pcs_component_path, version=exists_version
+    )
+    assert exists_path.exists()
+    # pcs/component.py does NOT exist in 07bc713
+    not_exists_version = "07bc71358b4360092b58d78f9eee6dc939e90b10"
+    not_exists_path = aos_repo.get_local_file_path(
+        pcs_component_path, version=not_exists_version
+    )
+    assert not not_exists_path.exists()
