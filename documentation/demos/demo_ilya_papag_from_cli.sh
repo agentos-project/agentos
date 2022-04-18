3ffef71657c3c35e036c04339d8a68217ebe6e48
Andy Konwinski <andykonwinski@gmail.com>
2022-04-18 16:57:25 -0700

    tweak demo from cli

--- documentation/demos/demo_ilya_papag_from_cli.sh
+++ documentation/demos/demo_ilya_papag_from_cli.sh
@@ -4,4 +4,6 @@ cd ../..  # cd to agentos root
 PAPAG_DIR=./example_agents/papag
 
 agentos freeze agent --registry-file $PAPAG_DIR/components.yaml > /tmp/papag-components.yaml
-agentos run agent --registry-file /tmp/papag-components.yaml --entry-point learn --arg-set-file $PAPAG_DIR/a2c_pong_args.yaml
+agentos run agent --registry-file /tmp/papag-components.yaml --entry-point learn --arg-set-file $PAPAG_DIR/a2c_pong_args.yaml > output.txt
+AGENT_RUN_ID=$(grep -Eo 'AgentRun ([0-9,a-f]+)' output.txt|grep -o ' [0-9,a-f]\+')
+USE_LOCAL_SERVER=True agentos publish-run $AGENT_RUN_ID
