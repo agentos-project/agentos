eac7aeba6e440e82c541d9919fbb93b5b45e7ee6
Andy Konwinski <andykonwinski@gmail.com>
2022-05-25 14:59:00 -0700

    WIP partial port of PAPAG example agent -- not working yet

--- tests/example_agents/test_papag.py
+++ tests/example_agents/test_papag.py
@@ -2,26 +2,22 @@ from agentos.cli import run
 from tests.utils import PAPAG_AGENT_DIR, run_test_command
 
 # Use CartPole because Atari ROM licensing issues break tests on Windows.
-test_args = [
-    "papag_agent",
-    "--use-outer-env",
-    "-Anum_env_steps=1",
-    "-Anum_processes=1",
-]
+test_args = ["papag_agent", "-K {'num_env_steps': 1, 'num_processes': 1}"]
 
 test_kwargs = {
     "--registry-file": str(PAPAG_AGENT_DIR / "components.yaml"),
-    "--arg-set-file": str(PAPAG_AGENT_DIR / "a2c_cartpole_args.yaml"),
+    "--registry-file": str(PAPAG_AGENT_DIR / "a2c_cartpole_args.yaml"),
+    "--arg-set-id": "a2c_cartpole_args"
 }
 
 
 def test_papag_agent_evaluate():
     kwargs = {k: v for k, v in test_kwargs.items()}
-    kwargs["--entry-point"] = "evaluate"
+    kwargs["--function-name"] = "evaluate"
     run_test_command(cmd=run, cli_args=test_args, cli_kwargs=kwargs)
 
 
 def test_papag_agent_learn():
     kwargs = {k: v for k, v in test_kwargs.items()}
-    kwargs["--entry-point"] = "learn"
+    kwargs["--function-name"] = "learn"
     run_test_command(cmd=run, cli_args=test_args, cli_kwargs=kwargs)
