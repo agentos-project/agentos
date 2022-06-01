4896d4fadc50ae6427545b4ee02a92bb23adfd51
Andy Konwinski <andykonwinski@gmail.com>
2022-05-31 21:07:58 -0700

    fix bugs in papag component registries

--- tests/example_agents/test_papag.py
+++ tests/example_agents/test_papag.py
@@ -3,7 +3,7 @@ from tests.utils import PAPAG_AGENT_DIR, run_test_command
 
 # Use CartPole because Atari ROM licensing issues break tests on Windows.
 test_args = [
-    "a2c_papag_agent",
+    "a2c_cartpole_papag_agent",
     "--arg-set-kwargs", "{'num_env_steps': 1, 'num_processes': 1}",
     "--registry-file", str(PAPAG_AGENT_DIR / "components.yaml"),
     "--registry-file", str(PAPAG_AGENT_DIR / "a2c_cartpole_args.yaml"),
@@ -13,7 +13,7 @@ test_args = [
 def test_papag_agent_evaluate(cli_runner):
     kwargs = {
         "--function-name": "evaluate",
-        "--arg-set-id": "a2c_cartpole_eval_args",
+        "--arg-set-id": "evaluation_args",
     }
     run_test_command(cli_runner, run, cli_args=test_args, cli_kwargs=kwargs)
 
