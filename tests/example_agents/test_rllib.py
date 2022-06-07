8d48741294fa5bde4ece7a7c47fc62e6873e9d2a
Andy Konwinski <andykonwinski@gmail.com>
2022-06-06 22:12:34 -0700

    fix incorrect test skip syntax

--- tests/example_agents/test_rllib.py
+++ tests/example_agents/test_rllib.py
@@ -7,14 +7,14 @@ test_args = ["agent"]
 test_kwargs = {"--registry-file": str(RLLIB_AGENT_DIR / "components.yaml")}
 
 
-@pytest.skip()
+@pytest.mark.skip()
 def test_rllib_agent_evaluate(cli_runner):
     kwargs = {k: v for k, v in test_kwargs.items()}
     kwargs["--function-name"] = "evaluate"
     run_test_command(cli_runner, run, cli_args=test_args, cli_kwargs=kwargs)
 
 
-@pytest.skip()
+@pytest.mark.skip()
 def test_rllib_agent_learn(cli_runner):
     kwargs = {k: v for k, v in test_kwargs.items()}
     kwargs["--function-name"] = "learn"
