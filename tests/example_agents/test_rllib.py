d037bb6a8f0c767bb1afb96f20236975af26e99f
Andy Konwinski <andykonwinski@gmail.com>
2022-06-15 17:18:22 -0700

    uncomment and fix rllib tests. uncomment test_gh_sb3 which is working.

--- tests/example_agents/test_rllib.py
+++ tests/example_agents/test_rllib.py
@@ -7,14 +7,12 @@ test_args = ["agent"]
 test_kwargs = {"--registry-file": str(RLLIB_AGENT_DIR / "components.yaml")}
 
 
-@pytest.mark.skip()
 def test_rllib_agent_evaluate(cli_runner):
     kwargs = {k: v for k, v in test_kwargs.items()}
     kwargs["--function-name"] = "evaluate"
     run_test_command(cli_runner, run, cli_args=test_args, cli_kwargs=kwargs)
 
 
-@pytest.mark.skip()
 def test_rllib_agent_learn(cli_runner):
     kwargs = {k: v for k, v in test_kwargs.items()}
     kwargs["--function-name"] = "learn"
