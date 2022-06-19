9180aceb16a7a17b0a97c44df0dda2aab8d69e53
Nick Jalbert <nick@jalbert.io>
2022-06-19 15:52:45 +0200

    wip

--- web/registry/tests/test_registry.py
+++ web/registry/tests/test_registry.py
@@ -1,11 +1,9 @@
-# import yaml
-# import json
 from pathlib import Path
 
 from django.core.files.base import File
 from django.test import Client, LiveServerTestCase
 from django.urls import reverse
-# from registry.models import Component
+from registry.models import Component
 from registry.tests.utils import load_or_create_fixture
 
 
@@ -16,52 +14,28 @@ class RegistryTestCases(LiveServerTestCase):
         self.static_dir = Path(__file__).parent / "static"
 
     def test_run_upload_artifact(self):
-        self.assertFalse(bool(self.run.artifact_tarball))
+        component = Component.objects.all()[0]
+        self.assertFalse(bool(component.artifact_tarball))
         url = reverse(
-            "run-upload-artifact", kwargs={"pk": self.run.identifier}
+            "component-upload-artifact", kwargs={"pk": component.identifier}
         )
         tarball = open(self.static_dir / "test_artifacts.tar.gz", "rb")
         response = self.client.post(url, {"tarball": tarball})
         self.assertEqual(response.status_code, 200)
-        self.run.refresh_from_db()
-        self.assertTrue(bool(self.run.artifact_tarball))
+        component.refresh_from_db()
+        self.assertTrue(bool(component.artifact_tarball))
 
     def test_run_download_artifact(self):
+        component = Component.objects.all()[0]
         with open(self.static_dir / "test_artifacts.tar.gz", "rb") as file_in:
-            self.run.artifact_tarball.save(
+            component.artifact_tarball.save(
                 "test_artifacts.tar.gz", File(file_in)
             )
-            self.run.save()
+            component.save()
         url = reverse(
-            "run-download-artifact", kwargs={"pk": self.run.identifier}
+            "component-download-artifact", kwargs={"pk": component.identifier}
         )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers["Content-Type"], "application/gzip")
         self.assertIn("Content-Disposition", response.headers)
-
-    # TODO: update this test once we've finished reworking ingest_spec
-    #       to be ingest_registry (it is moving out from under the Module
-    #       model/view).
-    # def test_component_replace(self):
-    #     self.assertEqual(Module.objects.count(), 1)
-    #     url = reverse("component-ingest-spec")
-    #     fail_path = self.static_dir / "test_component_replace_fail.yaml"
-    #     fail_response = self.client.post(
-    #         url, {"components.yaml": open(fail_path)}
-    #     )
-    #     self.assertEqual(fail_response.status_code, 400)
-    #     self.assertEqual(Module.objects.count(), 1)
-    #     component = Module.objects.get(id=self.component.id)
-    #     self.assertEqual(self.component.repo.id, component.repo.id)
-    #     self.assertEqual(self.component.file_path, component.file_path)
-    #     self.assertEqual(self.component.name, component.name)
-    #     self.assertEqual(self.component.instantiate, component.instantiate)
-    #     success_path = (
-    #        self.static_dir / "test_component_replace_success.yaml"
-    #     )
-    #     success_response = self.client.post(
-    #         url, {"components.yaml": open(success_path)}
-    #     )
-    #     self.assertEqual(success_response.status_code, 200)
-    #     self.assertEqual(Module.objects.count(), 2)
--- web/registry/views.py
+++ web/registry/views.py
@@ -1,7 +1,10 @@
 from django.db import transaction
+from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .models import Component
@@ -29,6 +32,29 @@ class ComponentViewSet(viewsets.ModelViewSet):
         component = Component.create_from_request_data(request.data)
         serialized = ComponentSerializer(component)
         return Response(serialized.data)
+
+    @action(detail=True, methods=["POST"], url_name="upload-artifact")
+    @transaction.atomic
+    def upload_artifact(self, request: Request, pk=None) -> Response:
+        component = self.get_object()
+        file_name = "component{component.identifier}_artifacts.tar.gz"
+        component.artifact_tarball.save(file_name, request.data["tarball"])
+        component.save()
+        return Response(ComponentSerializer(component).data)
+
+    @action(detail=True, methods=["GET"], url_name="download-artifact")
+    def download_artifact(self, request: Request, pk=None) -> Response:
+        component = self.get_object()
+        if not component.artifact_tarball:
+            raise ValidationError(
+                f"No files associated with Component {component.identifier}"
+            )
+        response = HttpResponse(
+            component.artifact_tarball.open(), content_type="application/gzip"
+        )
+        disposition = f"attachment; filename={component.artifact_tarball.name}"
+        response["Content-Disposition"] = disposition
+        return response
 
 
 def _get_from_list(name, component_list):
